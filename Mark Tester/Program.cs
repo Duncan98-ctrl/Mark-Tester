eddb84f93366a7290f7b1e22453b6b1236a56220
Arno Potgieter <arnopotgieter11@gmail.com>
2021-08-18 15:10:17 +0200

    Add project files.

---
+++ Mark Tester/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Mark_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            
+            while(loop)
+            {
+                Console.WriteLine("<---- Welcome to Mark Tester ---->");
+
+                Console.Write("Enter the total: ");
+                float total = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(total);
+
+                Console.Write("Enter studentMark: ");
+                float studentMark = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(studentMark);
+
+                float number = (studentMark / total) * 100;
+                Console.WriteLine(number);
+
+                Console.WriteLine("Student Mark is " + number);
+
+                Console.Write("Do you want to enter a new student mark? Y/N: ");
+                string answer = Console.ReadLine();
+
+                if(answer == "Y" || answer == "y")
+                {
+                    loop = true;
+                }
+                else if(answer == "N" || answer == "n")
+                {
+                    loop = false;
+                }
+            }
+        }
+    }
+}
